ced2e1e65997c6c4c91555c31f676adbc4dcddf9
Mohamed Mostafa <medomado201529@gmail.com>
2024-01-08 09:27:34 +0100

    handel project

--- next.config.js
+++ next.config.js
@@ -2,9 +2,27 @@
 const nextConfig = {
   reactStrictMode: true,
   i18n: {
-    locales: ['en', 'ar'],
-    defaultLocale: 'en',
-},
-}
+    locales: ["en", "ar"],
+    defaultLocale: "en",
+  },
+  images: {
+    domains: ["adminlaundry.razinsoft.com", "localhost"], //Domain of image host
+  },
+  // /app/public
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "adminlaundry.razinsoft.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+    // loader: 'akamai',
+    // unoptimized: true,
+    // loader: "custom",
+    // loaderFile: "./src/imageloader.js",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
