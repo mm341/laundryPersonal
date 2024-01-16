7e72e6351019eee6666d8233f3da7adb9f12702a
Mohamed Mostafa <medomado201529@gmail.com>
2024-01-16 15:26:43 +0100

    handel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -163,6 +163,7 @@ export default function App({
                   </Box>
                 </Box>
                 <Box
+                  style={{ overflowX: "hidden" }}
                   sx={{
                     display: previewLoader ? "none" : "block",
                   }}
