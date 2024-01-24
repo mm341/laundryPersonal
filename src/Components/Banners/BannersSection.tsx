5bc7d938c502cd2bfcf754a509d4df46509c404d
Mohamed Mostafa <medomado201529@gmail.com>
2024-01-24 13:30:38 +0100

    handel

--- src/Components/Banners/BannersSection.tsx
+++ src/Components/Banners/BannersSection.tsx
@@ -11,19 +11,28 @@ const BannersSection = () => {
   return (
     banners?.length > 0 && (
       <Carousel
-      // sx={
-      //   '.mui-style-1m9128y' {
-      //     text-align: center !important;
-      //     margin-top: 25px !important;
-        
-      // }}
+        // sx={
+        //   '.mui-style-1m9128y' {
+        //     text-align: center !important;
+        //     margin-top: 25px !important;
+
+        // }}
+        sx={{
+          // listStyleType: 'disc',
+          // pl: 4,
+
+          "& .mui-style-1m9128y": {
+            textAlign: "center !important",
+            marginTop: "25px !important",
+          },
+        }}
         animation={"slide"}
         duration={200}
         swipe={true}
         navButtonsAlwaysVisible={true}
       >
         {banners?.map((banner, i: number) => (
-          <Box key={i}  sx={{ width: "100%" }}>
+          <Box key={i} sx={{ width: "100%" }}>
             <BannerCard banner={banner} />
           </Box>
         ))}
