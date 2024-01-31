4e98e5c101f738ccd478016315de9a48b26296f0
Mohamed Mostafa <medomado201529@gmail.com>
2024-01-31 08:53:42 +0100

    handel project

--- src/Components/AuthBox/auth/SignUpValidation.ts
+++ src/Components/AuthBox/auth/SignUpValidation.ts
@@ -10,10 +10,10 @@ const SignUpvalidation = () => {
     mobile: Yup.string()
       .required(t("Please give a phone number"))
       .min(12, t("number must be 12 digits"))
-      .matches(
-        /^(201|01|00201)[0-2,5]{1}[0-9]{8}$|^(009665|9665|9665|05|5)(5|0|3|6|4|9|1|8|7)([0-9]{7})$/,
-        t("The phone format is invalid.")
-      ),
+      // .matches(
+      //   /^(201|01|00201)[0-2,5]{1}[0-9]{8}$|^(009665|9665|9665|05|5)(5|0|3|6|4|9|1|8|7)([0-9]{7})$/,
+      //   t("The phone format is invalid.")
+      // ),
   });
 };
 
--- src/Components/AuthBox/auth/sign-in/SignIn.tsx
+++ src/Components/AuthBox/auth/sign-in/SignIn.tsx
@@ -65,10 +65,10 @@ const SignInPage = ({ setModalFor, handleClose, modalFor }: SignModel) => {
       mobile: Yup.string()
         .required(t("Please give a phone number"))
         .min(12, t("number must be 12 digits"))
-        .matches(
-          /^(201|01|00201)[0-2,5]{1}[0-9]{8}$|^(009665|9665|9665|05|5)(5|0|3|6|4|9|1|8|7)([0-9]{7})$/,
-          t("The phone format is invalid.")
-        ),
+        // .matches(
+        //   /^(201|01|00201)[0-2,5]{1}[0-9]{8}$|^(009665|9665|9665|05|5)(5|0|3|6|4|9|8|7)([0-9]{7})$/,
+        //   t("The phone format is invalid.")
+        // ),
     }),
     onSubmit: async (values: { mobile: string | undefined }) => {
       try {
