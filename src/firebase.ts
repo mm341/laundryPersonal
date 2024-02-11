e5471accfff272297e377a09f3d39319e55770a2
Mohamed Mostafa <medomado201529@gmail.com>
2024-02-11 15:45:35 +0100

    def

--- src/firebase.ts
+++ src/firebase.ts
@@ -34,35 +34,35 @@ const messaging = (async () => {
   }
 })();
 
-export const fetchToken = async (setTokenFound, setFcmToken) => {
-  return getToken(await messaging, {
-    vapidKey:
-      "BI91LwgqdJPFq0jOyHSbaPTbVlwYXsSpFf7Md5ixJ16wvbfvSqI51tcBld878DfU1iKkJFWXASaiP3Ybpp2ICK0",
-  })
-    .then((currentToken) => {
-      if (currentToken) {
-        setTokenFound(true);
-        setFcmToken(currentToken);
+// export const fetchToken = async (setTokenFound, setFcmToken) => {
+//   return getToken(await messaging, {
+//     vapidKey:
+//       "BI91LwgqdJPFq0jOyHSbaPTbVlwYXsSpFf7Md5ixJ16wvbfvSqI51tcBld878DfU1iKkJFWXASaiP3Ybpp2ICK0",
+//   })
+//     .then((currentToken) => {
+//       if (currentToken) {
+//         setTokenFound(true);
+//         setFcmToken(currentToken);
 
-        // Track the token -> client mapping, by sending to backend server
-        // show on the UI that permission is secured
-      } else {
-        setTokenFound(false);
-        setFcmToken();
-        // shows on the UI that permission is required
-      }
-    })
-    .catch((err) => {
-      // catch error while creating client token
-    });
-};
+//         // Track the token -> client mapping, by sending to backend server
+//         // show on the UI that permission is secured
+//       } else {
+//         setTokenFound(false);
+//         setFcmToken();
+//         // shows on the UI that permission is required
+//       }
+//     })
+//     .catch((err) => {
+//       // catch error while creating client token
+//     });
+// };
 
-export const onMessageListener = async () =>
-  new Promise((resolve) =>
-    (async () => {
-      const messagingResolve = await messaging;
-      onMessage(messagingResolve, (payload) => {
-        resolve(payload);
-      });
-    })()
-  );
+// export const onMessageListener = async () =>
+//   new Promise((resolve) =>
+//     (async () => {
+//       const messagingResolve = await messaging;
+//       onMessage(messagingResolve, (payload) => {
+//         resolve(payload);
+//       });
+//     })()
+//   );
