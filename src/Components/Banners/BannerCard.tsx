485a4e60123b14fcf0c9f827e0612c0b063032f3
Mohamed Mostafa <medomado201529@gmail.com>
2024-02-28 13:53:47 +0100

    aef

--- src/Components/Banners/BannerCard.tsx
+++ src/Components/Banners/BannerCard.tsx
@@ -2,37 +2,77 @@ import React from "react";
 
 import { useSelector } from "react-redux";
 
-import { Box, useMediaQuery, useTheme } from "@mui/material";
-import Image from "next/image";
+import { Box, Typography, useMediaQuery, useTheme } from "@mui/material";
+import backGroundImg from "../../../public/products/baneerBackground.svg";
 import { Banners } from "@/interfaces/ServiceBanners";
+import {
+  GlobalDisplayFlexBox,
+  GlobalDisplayFlexColumnBox,
+} from "@/styles/PublicStyles";
 
 const BannerCard = ({ banner }: { banner: Banners }) => {
   // hooks
-  const theme=useTheme()
-  const issmall = useMediaQuery(theme.breakpoints.down("md"));
+  const theme = useTheme();
+  const issmall = useMediaQuery(theme.breakpoints.down("lg"));
+  const isxsmall = useMediaQuery(theme.breakpoints.down("md"));
+
+  
   return (
-    <Box
-      sx={{
-        width: "100%",
-      
-        mt: { md: "0", xs: "20px" },
-      }}
-    >
-      {banner?.image_path && (
-        <img
-          src={banner?.image_path}
-          alt={"bannerImg"}
-         
-          style={{
-            height: !issmall?"447px":"250px",
-            borderRadius: "8px",
-            
-            width:"100%"
+    <GlobalDisplayFlexBox sx={{ position: "relative" }}>
+      <GlobalDisplayFlexBox
+        style={{ flexDirection: "row", height: "100%",gap:"50px" }}
+        sx={{
+          width: "100%",
+          position: "absolute",
+          // mt: { md: "0", xs: "20px" },
+          gap: "50px",
+          justifyContent: "center",
+        }}
+      >
+        {banner?.image_path && (
+          <img
+            src={banner?.image_path}
+            alt={"bannerImg"}
+            style={{
+              height:!isxsmall?"200px":"60px",
+              borderRadius: "8px",
+
+              width:  "250px",
+            }}
+            loading="lazy"
+          />
+        )}
+        <GlobalDisplayFlexColumnBox
+          sx={{
+            width: { sm: "50%", xs: "100%" },
+            alignItems: "center",
+            transform: {
+              md: "translate(120px,15px)",
+              xs: "translate(0px,0px)",
+            },
           }}
-          loading="lazy"
-        />
-      )}
-    </Box>
+        >
+          <Typography
+            sx={{
+              fontSize: { lg: "70px", md: "25px", xs: "18px" },
+              fontWeight: "1000",
+              color: theme.palette.primary.main,
+            }}
+          >
+            {banner?.title}
+          </Typography>
+          <Typography
+            sx={{
+              fontSize: { lg: "32px", md: "20px", xs: "12px" },
+              fontWeight: "300",
+            }}
+          >
+            {banner?.description}
+          </Typography>
+        </GlobalDisplayFlexColumnBox>
+      </GlobalDisplayFlexBox>
+      <img style={{ width: "100%" }} src={backGroundImg?.src} loading="lazy" />
+    </GlobalDisplayFlexBox>
   );
 };
 
